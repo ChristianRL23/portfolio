844a0bb827b4b4f117d6af352e588103883bb9d2
Christian Lara <christianramirezlara23@gmail.com>
2022-01-16 21:27:27 -0600

    Add navbar to the hero

--- src/layout/Hero/Hero.jsx
+++ src/layout/Hero/Hero.jsx
@@ -4,11 +4,21 @@ import './Hero.scss';
 const Hero = () => {
   return (
     <header className="hero">
-      <Logo />
       <nav className="navbar">
-        <a href="#">Acerca</a>
-        <a href="#">Proyectos</a>
-        <a href="#">Contacto</a>
+        <div className="navbar__left">
+          <Logo />
+        </div>
+        <div className="navbar__right">
+          <a className="navbar__right-link" href="#">
+            Acerca
+          </a>
+          <a className="navbar__right-link" href="#">
+            Proyectos
+          </a>
+          <a className="navbar__right-link" href="#">
+            Contacto
+          </a>
+        </div>
       </nav>
     </header>
   );
