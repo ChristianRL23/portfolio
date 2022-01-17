466a61fcd07746895712785b1bbec2d32400eab1
Christian Lara <christianramirezlara23@gmail.com>
2022-01-17 08:05:45 -0600

    Initialize component and add some HTML content

---
+++ src/layout/Hero/About.jsx
@@ -0,0 +1,33 @@
+import './About.scss';
+
+const About = () => {
+  return (
+    <section>
+      <h2>Acerca de mi</h2>
+      <div>
+        <h4>¿Quién soy yo?</h4>
+        <p>
+          Soy una persona responsable y profesional que le gusta el diseño web y
+          ser minucioso con los detalles a fin de crear interfaces bonitas y
+          atractivas.
+        </p>
+        <p>
+          Comencé a estudiar tecnologías relacionados con el desarrollo web a
+          finales de 2020. Desde entonces no he dejado de prepararme para ser
+          desarrollador Front-End.
+        </p>
+        <p>
+          Actualmente me encuentro buscando mi priemera oportunidad laboral por
+          lo que si es de su agrado la manera en la que trabajo estoy disponible
+          en todo momento para platicar con usted.
+        </p>
+      </div>
+      <div>
+        <h4>Tecnologías que uso</h4>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
