568b958b1d5d9990f1fdbf4532a72fa73b32aaaf
Christian Lara <christianramirezlara23@gmail.com>
2022-01-17 08:46:49 -0600

    Style the information box

--- src/layout/Hero/About.jsx
+++ src/layout/Hero/About.jsx
@@ -6,17 +6,17 @@ const About = () => {
       <h2 className="about__title">Acerca de mi</h2>
       <div className="about__information">
         <h4 className="information__subtitle">¿Quién soy yo?</h4>
-        <p>
+        <p className="information__text">
           Soy una persona responsable y profesional que le gusta el diseño web y
           ser minucioso con los detalles a fin de crear interfaces bonitas y
           atractivas.
         </p>
-        <p>
+        <p className="information__text">
           Comencé a estudiar tecnologías relacionados con el desarrollo web a
           finales de 2020. Desde entonces no he dejado de prepararme para ser
           desarrollador Front-End.
         </p>
-        <p>
+        <p className="information__text">
           Actualmente me encuentro buscando mi priemera oportunidad laboral por
           lo que si es de su agrado la manera en la que trabajo estoy disponible
           en todo momento para platicar con usted.
