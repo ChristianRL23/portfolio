b477d43dc0840515e1c8390b07146d4ba5baba88
Christian Lara <christianramirezlara23@gmail.com>
2022-01-16 21:07:20 -0600

    Create logo component and use it

--- src/layout/Hero/Hero.jsx
+++ src/layout/Hero/Hero.jsx
@@ -1,7 +1,17 @@
+import Logo from '../../components/Logo/Logo';
 import './Hero.scss';
 
 const Hero = () => {
-  return <header className="hero"></header>;
+  return (
+    <header className="hero">
+      <Logo />
+      <nav className="navbar">
+        <a href="#">Acerca</a>
+        <a href="#">Proyectos</a>
+        <a href="#">Contacto</a>
+      </nav>
+    </header>
+  );
 };
 
 export default Hero;
