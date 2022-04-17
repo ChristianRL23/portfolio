090028001625ee0ad578e65eb2a5dc76ad235d4a
Christian Lara <christianramirezlara23@gmail.com>
2022-04-16 19:41:23 -0500

    Make responsive the portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,7 @@
+import hamburgerIcon from './hamburger.svg';
+import { slide as Burger, Item } from 'burger-menu';
+import 'burger-menu/lib/index.css';
+import { useMediaQuery } from 'react-responsive';
 import { useState, useEffect } from 'react';
 import Logo from '../Logo/Logo';
 import './Navbar.scss';
@@ -5,6 +9,10 @@ import './Navbar.scss';
 const Navbar = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
   const [navbarTransparent, setNavbarTransparent] = useState(true);
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const isTablet = useMediaQuery({
+    query: '(max-width: 700px)',
+  });
 
   const handleScroll = () => {
     const position = window.pageYOffset;
@@ -13,7 +21,6 @@ const Navbar = () => {
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, { passive: true });
-    console.log(scrollPosition);
     if (scrollPosition >= 75 && navbarTransparent === true) {
       setNavbarTransparent(false);
     } else if (scrollPosition < 75 && navbarTransparent === false) {
@@ -24,22 +31,65 @@ const Navbar = () => {
     };
   }, [scrollPosition]);
 
+  const scrollToSection = (e) => {
+    const sectionId = '#' + e.text.charAt(0).toLowerCase() + e.text.slice(1);
+    setSidebarIsOpen(false);
+    window.location.href = sectionId;
+  };
+
   return (
     <nav className={`navbar${navbarTransparent ? '--transparent' : ''}`}>
       <div className="navbar__left">
         <Logo />
       </div>
-      <div className="navbar__right">
-        <a className="navbar__right-link" href="#about">
-          About
-        </a>
-        <a className="navbar__right-link" href="#projects">
-          Projects
-        </a>
-        <a className="navbar__right-link" href="#contact">
-          Contact
-        </a>
-      </div>
+      {isTablet ? (
+        <>
+          <div onClick={() => setSidebarIsOpen(true)}>
+            <img
+              className="navbar__hamburger-icon"
+              src={hamburgerIcon}
+              alt="Hamburger icon"
+            />
+          </div>
+          <Burger
+            width={200}
+            className="burger-menu"
+            isOpen={sidebarIsOpen}
+            selectedKey={'entry'}
+            onClose={() => setSidebarIsOpen(false)}
+          >
+            <>
+              <Item
+                onClick={(e) => scrollToSection(e)}
+                itemKey="About"
+                text="About"
+              ></Item>
+              <Item
+                onClick={(e) => scrollToSection(e)}
+                itemKey="Projects"
+                text="Projects"
+              ></Item>
+              <Item
+                onClick={(e) => scrollToSection(e)}
+                itemKey="Contact"
+                text="Contact"
+              ></Item>
+            </>
+          </Burger>
+        </>
+      ) : (
+        <div className="navbar__right">
+          <a className="navbar__right-link" href="#about">
+            About
+          </a>
+          <a className="navbar__right-link" href="#projects">
+            Projects
+          </a>
+          <a className="navbar__right-link" href="#contact">
+            Contact
+          </a>
+        </div>
+      )}
     </nav>
   );
 };
--- src/layout/Hero/Hero.jsx
+++ src/layout/Hero/Hero.jsx
@@ -1,26 +1,9 @@
 import Button from '../../components/Button/Button';
-import Logo from '../../components/Logo/Logo';
 import './Hero.scss';
 
 const Hero = () => {
   return (
     <header className="hero">
-      {/* <nav className="navbar">
-        <div className="navbar__left">
-          <Logo />
-        </div>
-        <div className="navbar__right">
-          <a className="navbar__right-link" href="#">
-            About
-          </a>
-          <a className="navbar__right-link" href="#">
-            Projects
-          </a>
-          <a className="navbar__right-link" href="#">
-            Contact
-          </a>
-        </div>
-      </nav> */}
       <div className="hero__title">
         <h1 className="hero__title-name">Christian Uriel Ramírez Lara</h1>
         <h2 className="hero__title-role">Front-End Developer</h2>
